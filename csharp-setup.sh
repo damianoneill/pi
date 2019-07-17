1357b96719f1c2767f2c783f3f7f9897cbb427b7
damianoneill <damian.oneill@gmail.com>
2019-07-17 09:06:06 +0100

    download to /tmp

--- csharp-setup.sh
+++ csharp-setup.sh
@@ -8,7 +8,7 @@ curl -L https://code.headmelted.com/installers/apt.sh | sudo bash
 # https://github.com/headmelted/codebuilds/issues/67
 sudo apt-get -y --allow-downgrades install code-oss=1.29.0-1539702286
 sudo apt-mark hold code-oss
-sudo apt-mark unhold code-oss
+# sudo apt-mark unhold code-oss
 
 # install vs code C# support
 code-oss --install-extension ms-vscode.csharp --force
@@ -18,8 +18,8 @@ sudo apt-get -y install curl libunwind8 gettext
 
 # install dotnet sdk
 # see https://dotnet.microsoft.com/download/thank-you/dotnet-sdk-2.2.301-linux-arm32-binaries contents for url below
-curl -L https://download.visualstudio.microsoft.com/download/pr/4bc4d8e7-e736-4323-b82c-f75559502e9c/582e01f7b7a67cd23a22e5bfff317f10/dotnet-sdk-2.2.301-linux-arm.tar.gz --output dotnet-sdk-2.2.301-linux-arm.tar.gz
-mkdir -p ~/dotnet && tar zxf dotnet-sdk-2.2.301-linux-arm.tar.gz -C ~/dotnet
+curl -L https://download.visualstudio.microsoft.com/download/pr/4bc4d8e7-e736-4323-b82c-f75559502e9c/582e01f7b7a67cd23a22e5bfff317f10/dotnet-sdk-2.2.301-linux-arm.tar.gz --output /tmp/dotnet-sdk-2.2.301-linux-arm.tar.gz
+mkdir -p ~/dotnet && tar zxf /tmp/dotnet-sdk-2.2.301-linux-arm.tar.gz -C ~/dotnet
 grep -qxF 'export DOTNET_ROOT=$HOME/dotnet' ~/.bashrc || echo 'export DOTNET_ROOT=$HOME/dotnet' >>~/.bashrc
 grep -qxF 'export PATH=$PATH:$HOME/dotnet' ~/.bashrc || echo 'export PATH=$PATH:$HOME/dotnet' >>~/.bashrc
 source ~/.bashrc
