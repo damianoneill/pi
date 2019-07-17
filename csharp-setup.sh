2dea46389ae8ef376ff1a6049f7f21691388b174
damianoneill <damian.oneill@gmail.com>
2019-07-17 08:39:34 +0100

    name the downloaded file

--- csharp-setup.sh
+++ csharp-setup.sh
@@ -18,7 +18,7 @@ apt-get install curl libunwind8 gettext
 
 # install dotnet sdk
 # see https://dotnet.microsoft.com/download/thank-you/dotnet-sdk-2.2.301-linux-arm32-binaries contents for url below
-curl -L https://download.visualstudio.microsoft.com/download/pr/4bc4d8e7-e736-4323-b82c-f75559502e9c/582e01f7b7a67cd23a22e5bfff317f10/dotnet-sdk-2.2.301-linux-arm.tar.gz
+curl -L https://download.visualstudio.microsoft.com/download/pr/4bc4d8e7-e736-4323-b82c-f75559502e9c/582e01f7b7a67cd23a22e5bfff317f10/dotnet-sdk-2.2.301-linux-arm.tar.gz --output dotnet-sdk-2.2.301-linux-arm.tar.gz
 mkdir -p $HOME/dotnet && tar zxf dotnet-sdk-2.2.301-linux-arm.tar.gz -C $HOME/dotnet
 grep -qxF 'export DOTNET_ROOT=$HOME/dotnet' ~/.bashrc || echo 'export DOTNET_ROOT=$HOME/dotnet' >>~/.bashrc
 grep -qxF 'export PATH=$PATH:$HOME/dotnet' ~/.bashrc || echo 'export PATH=$PATH:$HOME/dotnet' >>~/.bashrc
