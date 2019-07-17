80e7578aed0900da532527e1754d123b60fcce3c
damianoneill <damian.oneill@gmail.com>
2019-07-17 08:58:15 +0100

    force install

--- csharp-setup.sh
+++ csharp-setup.sh
@@ -11,14 +11,14 @@ sudo apt-mark hold code-oss
 sudo apt-mark unhold code-oss
 
 # install vs code C# support
-code-oss --install-extension ms-vscode.csharp
+code-oss --install-extension ms-vscode.csharp --force
 
 # install .net core dependencies
-sudo apt-get install curl libunwind8 gettext
+sudo apt-get -y install curl libunwind8 gettext
 
 # install dotnet sdk
 # see https://dotnet.microsoft.com/download/thank-you/dotnet-sdk-2.2.301-linux-arm32-binaries contents for url below
-curl -L https://download.visualstudio.microsoft.com/download/pr/4bc4d8e7-e736-4323-b82c-f75559502e9c/582e01f7b7a67cd23a22e5bfff317f10/dotnet-sdk-2.2.301-linux-arm.tar.gz --output dotnet-sdk-2.2.301-linux-arm.tar.gz --silent
+curl -L https://download.visualstudio.microsoft.com/download/pr/4bc4d8e7-e736-4323-b82c-f75559502e9c/582e01f7b7a67cd23a22e5bfff317f10/dotnet-sdk-2.2.301-linux-arm.tar.gz --output dotnet-sdk-2.2.301-linux-arm.tar.gz
 mkdir -p ~/dotnet && tar zxf dotnet-sdk-2.2.301-linux-arm.tar.gz -C ~/dotnet
 grep -qxF 'export DOTNET_ROOT=$HOME/dotnet' ~/.bashrc || echo 'export DOTNET_ROOT=$HOME/dotnet' >>~/.bashrc
 grep -qxF 'export PATH=$PATH:$HOME/dotnet' ~/.bashrc || echo 'export PATH=$PATH:$HOME/dotnet' >>~/.bashrc
