d64c9f0d01445fbc5cd7587b74ee0e67aa5dab80
damianoneill <damian.oneill@gmail.com>
2019-07-17 08:06:06 +0100

    allow downgrades

--- csharp-setup.sh
+++ csharp-setup.sh
@@ -6,6 +6,6 @@ wget https://packagecloud.io/headmelted/codebuilds/gpgkey -O - | sudo apt-key ad
 curl -L https://code.headmelted.com/installers/apt.sh | sudo bash
 
 # https://github.com/headmelted/codebuilds/issues/67
-apt-get install code-oss=1.29.0-1539702286
+apt-get -y --allow-downgrades install code-oss=1.29.0-1539702286
 apt-mark hold code-oss
 apt-mark unhold code-oss
